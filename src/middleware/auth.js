c4568a5d5424221e51534b111deb91ff6de3e89d
darshpriyadarshan <priyadarshanreddy.seelam@gmail.com>
2021-03-26 08:47:27 +0530

    update auth to work with ejs

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,19 +3,18 @@ const User = require('../models/user')
 
 const auth = async (req, res, next) => {
     try {
-        const token = req.header('Authorization').replace('Bearer ', '')
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const header_cookie = req.headers.cookie
+        token = header_cookie.substring(6)//removing token= from header cookie
+        const decoded = jwt.verify(token, 'thisistrail')
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
-
-        if (!user) {
+        if (!user)
             throw new Error()
-        }
-
         req.token = token
         req.user = user
         next()
     } catch (e) {
-        res.status(401).send({ error: 'Please authenticate.' })
+            console.log(e)
+            res.send("<h1>You will have to login first!!!</h1>")
     }
 }
 
