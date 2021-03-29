a376ed9512dd573dfae552cf2f718181142b047b
darshpriyadarshan <priyadarshanreddy.seelam@gmail.com>
2021-03-29 21:42:46 +0530

    added sending email feature using cron job and mailgun

---
+++ src/emails/sendemail.js
@@ -0,0 +1,47 @@
+const Birthday = require('../models/birthday')
+const cron = require('node-cron')
+require('../db/mongoose')
+const mailgun = require('mailgun-js')({ apiKey: process.env.API_KEY, domain: process.env.DOMAIN })
+
+isTomorrow = (date1, date2) => {
+    d1 = date1.split(' ')
+    d2 = date2.split(' ')
+
+    if (d1[0] == d2[0] && parseInt(d1[1], 10) - 1 == parseInt(d2[1], 10))
+        return true
+    return false
+}
+
+cron.schedule('0 0 9 * * *', async function () {
+    const formatter = new Intl.DateTimeFormat('en', { month: 'long', day: 'numeric' });
+    const list = []
+    const birthdays = await Birthday.find()
+    for (const birthday of birthdays) {
+        birthday.date2 = formatter.format(birthday.date)
+        await birthday.populate('createdBy').execPopulate()
+        if (isTomorrow(birthday.date2, formatter.format(Date.now())))
+            list.push({ 'email': birthday.createdBy.email,
+                        'senderName': birthday.createdBy.name,
+                        'name': birthday.name,
+                        'birthday': birthday.date2})
+    }
+    console.log(list)
+
+    for(const item of list){
+        const data = {
+            from: 'Excited User <me@samples.mailgun.org>',
+            to: item.email,
+            subject: 'Reminder: ' + item.name + '\'s birthday is tomorrow',
+            text: 'Hey ' + item.senderName + 'you have asked us to remind you of your friend' + item.name + '\'s birthday.' +
+                 'It\'s tomorrow on ' + item.birthday + '. Hope this helped'
+        }
+
+        mailgun.messages().send(data, (error, body) => {
+            console.log(body)
+            console.log(error)
+        })
+    }
+})
+
+
+
